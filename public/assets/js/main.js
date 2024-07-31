58a884f82621bf5d49a0f7697c73c88224250539
Ashwin <ashwinm.045@gmail.com>
2024-07-31 14:14:45 +0530

    Added sidebar foe selecting

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,4 +1,27 @@
+document.getElementById('addFieldsButton').addEventListener('click', function() {
+    const additionalFields = document.getElementById('additionalFields');
+    const isNewHostSelectorOpened = additionalFields.classList.toggle('show');
+    if(isNewHostSelectorOpened)
+        document.getElementById('hostSelect').disabled = true;
+    else
+        document.getElementById('hostSelect').disabled = false;
+  });
 
-$(document).ready( () => {
+  document.querySelectorAll('input[name="fileOrPassword"]').forEach(function(radio) {
+    radio.addEventListener('change', function() {
+      const fileUpload = document.getElementById('fileUpload');
+      const passwordInput = document.getElementById('passwordInput');
+      if (this.value === 'file') {
+        fileUpload.classList.remove('d-none');
+        passwordInput.classList.add('d-none');
+      } else {
+        fileUpload.classList.add('d-none');
+        passwordInput.classList.remove('d-none');
+      }
+    });
+  });
 
-})
+  const openSelectHostModal = ()=>{
+    toggleSidebar()
+    $('#test').modal('show')
+}
